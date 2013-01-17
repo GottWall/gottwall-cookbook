5784fd147e1f2f33947f70a1784a7364f63fd030
Alex <Alexandr.S.rus@gmail.com>
2013-01-17 12:27:22 +0400

    Fix provider attributes

--- resources/instance.rb
+++ resources/instance.rb
@@ -22,6 +22,7 @@ attribute :config, :kind_of => String
 attribute :host, :kind_of => String, :default => "0.0.0.0"
 attribute :port, :kind_of => Integer, :default => 9000
 attribute :workers, :kind_of => Integer, :default => 3
+attribute :template_name, :kind_of => String, :default => "gottwall"
 
 
 def initialize(*args)
