20b0f74fe7bc54259b8d7109770445e3753d63a4
Alex <Alexandr.S.rus@gmail.com>
2013-01-17 16:42:44 +0400

    Update version

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,7 +12,7 @@ default["gottwall"]["group"] = "gottwall"
 default["gottwall"]["config"] = "/etc/gottwall.conf.py"
 default["gottwall"]["virtualenv"] = "/var/www/gottwall"
 
-default["gottwall"]["version"] = "0.1.17"
+default["gottwall"]["version"] = "0.1.18"
 
 
 default["gottwall"]["storage"] = "gottwall.storages.RedisStorage"
